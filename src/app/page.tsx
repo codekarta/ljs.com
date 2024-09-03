146e51390711e05b1ce792e5715b8b1cb52a34db
Manish Bansal <manishbansal.dev@gmail.com>
2024-09-03 18:44:27 +0530

    updated code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Home() {
   return (
     <div className="flex justify-center">
-      <div>hello world demo</div>
+      <div>hello world demo sarang</div>
     </div>
   )
 }
